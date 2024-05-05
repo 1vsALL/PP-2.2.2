c10512eb550c4fcad6596e4c46fbd2093167850e
1vsALL <alikkv@mail.ru>
2024-05-05 11:02:06 +0300

    done

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,36 @@
+package web.model;
+
+public class Car {
+    private int id;
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
